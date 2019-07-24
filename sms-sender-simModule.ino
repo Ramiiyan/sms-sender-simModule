24edf3d788d0b204d6acf6ca1c366d34b9cd46bd
Ramiiyan <Sriraguhan.2017380@iit.ac.lk>
2019-07-24 10:37:57 +0530

    Updated

--- sms-sender-simModule.ino
+++ sms-sender-simModule.ino
@@ -1,15 +1,19 @@
-#define TINY_GSM_MODEM_SIM7000
+/*
+ * CREATED BY.RAMIYAN
+ */
+
+#define TINY_GSM_MODEM_SIM7000  //defining Your SimModule for TinyGSM library Purpose.
 #include <Arduino.h>
-#include <TinyGsmClient.h>
+#include <TinyGsmClient.h> //Install TimGsm Library
 #include <SoftwareSerial.h>
 #include <Wire.h>
 
-#define NumberD "+94777333702"
+#define NumberD "*************"  //Enter Sender Number with contry code.(DEFAULT)
 
 SoftwareSerial SerialGSM(7, 8);
 TinyGsm modem(SerialGSM);
 
-String number = "+94";
+String number = "***"; // Enter Your Country code For use as At command
 String ccid;
 void done();
 
@@ -17,17 +21,17 @@ void setup()
 {
   Serial.begin(115200);
   delay(300);
-  SerialGSM.begin(4800);
-  modem.setNetworkMode(13); // 38-LTE, 13-GSM
-  modem.setPreferredMode(2); // 2-NBIOT, 1-GSM
+  SerialGSM.begin(4800);      // 4800 is defalut sim7000 baud. change if you have any Exceptions
+  //modem.setNetworkMode(13); // 38-LTE, 13-GSM
+  //modem.setPreferredMode(2); // 2-NBIOT, 1-GSM
 
-  Serial.print(F("Finding Network..."));
-  modem.restart();
+  Serial.print(F("Finding Network...")); 
+  modem.restart();  
   delay(4000);
   Serial.print("Testing..");
   if (!modem.waitForNetwork())
   {
-    Serial.println(F("Network Fail."));
+    Serial.println(F("Network Fail."));  // Check your SIM7000 Module 
     while (true)
       ;
   }
@@ -36,14 +40,14 @@ void setup()
     Serial.println(F("Network identified."));
     Serial.print(F("Signal Strength : "));
     Serial.println(modem.getSignalQuality());
-    if (!modem.gprsConnect("dialogbb", "", ""))
+    if (!modem.gprsConnect("******* ", "", ""))  // use As "Sim APN", "userName", "password" //for default username, pwd will be null("","") 
     {
       Serial.println(F("GPRS Fail"));
     }
     else
     {
       Serial.println(F("GPRS connected."));
-      SerialGSM.println("AT+CCID");
+      SerialGSM.println("AT+CCID");  // AT command for getting CCID sim Number(printed on the sim) FOR sim7000
       done();
     }
   }
@@ -67,13 +71,17 @@ void done()
   Serial.print("CCID: ");
   Serial.println(ccid);
 
+// if you enter the number via AT command....
   if (Serial.available())
   {
     number += Serial.readString();
     Serial.print("number :");
     Serial.println(number);
     delay(1000);
-    modem.sendSMS(number, ccid);
+    modem.sendSMS(number, ccid);  //Sending message using TinyGSM library 
+    Serial.println("Sent !");
+  }else{
+    modem.sendSMS(NumberD, ccid); //Defaultly Sending message To Defined number above.
+    Serial.println("Sent !");
   }
-  modem.sendSMS(NumberD, ccid);
 }
